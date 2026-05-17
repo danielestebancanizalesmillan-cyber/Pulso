65f5eeaa29d272d0e9c25f2ccffcec5488365311
TheDani7w7 <elcrac244@gmail.com>
2026-05-16 22:04:24 -0500

    Fix: actualizacion de politicas de CSP para Google Translate gstatic y translate-pa

--- next.config.ts
+++ next.config.ts
@@ -19,13 +19,13 @@ const nextConfig: NextConfig = {
             // Remove 'unsafe-eval' as soon as debugging is complete.
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.pusher.com https://www.youtube-nocookie.com https://www.youtube.com https://unpkg.com https://translate.google.com https://translate.googleapis.com",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com https://translate.googleapis.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.pusher.com https://www.youtube-nocookie.com https://www.youtube.com https://unpkg.com https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com https://www.gstatic.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com https://translate.googleapis.com https://www.gstatic.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https: http:",
               "media-src 'self' blob: https:",
               "frame-src https://www.youtube-nocookie.com https://www.youtube.com",
-              "connect-src 'self' https: wss: blob:",
+              "connect-src 'self' https: wss: blob: https://translate-pa.googleapis.com",
             ].join("; ")
           },
         ],
